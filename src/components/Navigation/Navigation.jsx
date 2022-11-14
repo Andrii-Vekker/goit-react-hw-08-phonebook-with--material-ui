ef1fb1ef993ab813cadf3a71c1715534fbca6b4f
Andrii-Vekker <65786577+Andrii-Vekker@users.noreply.github.com>
2022-11-14 09:20:15 +0200

    fixed add and home page

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 import { isLogin } from "redux/auth/authSelectors";
 import { Typography } from "@mui/material";
 import { ToolNav } from "./Navigation.styled";
+import { NavLink } from "react-router-dom";
 
 export default function Navigation() {
 const isLoginUser = useSelector(isLogin)
@@ -12,7 +13,7 @@ const isLoginUser = useSelector(isLogin)
     return (
         <NavBar component="nav">
             <ToolNav>
-                <Typography h="6">Phonebook</Typography>
+                <Typography h="6"><NavLink to="/" style={{color:"#fff"}}>Phonebook</NavLink></Typography>
                 {isLoginUser ? <UserMenu /> : <UserAuth />}
             </ToolNav>
         </NavBar>
--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -1,12 +1,18 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux"; 
+import { isLogin } from "redux/auth/authSelectors";
+import { Navigate } from "react-router-dom";
 
 
 
 
 export default function HomePage() {
+    const isUserLogin = useSelector(isLogin);
   return (
       <div style={{ marginTop: "200px" }}>
-      <NavLink to="/login"><img src="https://png.pngtree.com/png-vector/20190628/ourlarge/pngtree-phone-icon-for-your-project-png-image_1520830.jpg" alt="phone" style={{backgroundColor:"inherit"}} /></NavLink>
+      {!isUserLogin ? <NavLink to="/login">
+        <img src="https://png.pngtree.com/png-vector/20190628/ourlarge/pngtree-phone-icon-for-your-project-png-image_1520830.jpg" alt="phone"  />
+      </NavLink> : <Navigate to="/contacts"/>}
       </div>
   )
 }
